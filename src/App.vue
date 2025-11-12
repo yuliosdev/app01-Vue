a2b4c20109a06df318c4220fe5f2a26a880f6f94
Yulios <yuliostch@gmail.com>
2025-11-12 11:53:42 -0500

    Agregar clase dinámica y estilos al componente App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
 const msg01 = "Bienvenido Yulios Dev!";
 const msg02 = "<h1>Comencemos con Vue3</h1>";
+const clase = "bg-red"
 </script>
 
 <template>
   <div>{{ msg01 }}</div>
   <div v-html="msg02"></div>
+  <div :class="clase">Este div tiene una clase dinámica</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.bg-red {
+  background-color: red;
+  color: white;
+  padding: 10px;
+}
+</style>
